276d588c5593c18a78d853c7f400af21fa2ee5b2
Marc-Antoine Ruel <maruel@gmail.com>
2022-05-10 10:39:38 -0400

    Still doesn't work

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -eu
 cd "$(dirname $0)"
-#python3 -m venv .
-#. ./bin/activate
-#pip install pipenv
-#pipenv install
+# pip install --upgrade pip
+# python3 -m venv .
+# . ./bin/activate
+# pip install pipenv
+if [ ! -f /usr/lib/x86_64-linux-gnu/libcairo.a ]; then
+  sudo apt install libcairo2-dev
+fi
+if [ ! -f /usr/lib/x86_64-linux-gnu/gobject-introspection ]; then
+  sudo apt install gobject-introspection
+fi
+pip install cython
+pip install pygobject==3.38.0
+pipenv install
 python3 -m emote
